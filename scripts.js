f96e921b1a346205ced9d817cda4f81fe681878c
kovacsbenc3 <73656785+kovacsbenc3@users.noreply.github.com>
2020-12-17 16:54:34 +0100

    Some Fun

--- scripts.js
+++ scripts.js
@@ -2,5 +2,8 @@ $("#1").click(()=>{$("body").append("<button>ACTIVATED</button>");
 });
 $("#2").click(()=>{$("#1").text("MISSION ACCOMPLISHED");
 });
-$("#3").click(()=>{$(".but").css("background-color", "red");
+$("#3").click(()=>{
+    var str = $("#textbox").val();
+    $(".but").css("background",str);
+    
 });
